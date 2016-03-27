8efb519c8f0922200b2a3aec903e58f26b2a7456
lw <cui@hellolw.com>
2016-03-27 11:13:24 +0800

    finish input bitstream

--- src/bitstream.cpp
+++ src/bitstream.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
+#include <cstdio>
 #include <bitset>
 #include <fstream>
 #include <string>
 using namespace std;
+
+class ibstream {
+	static const int SIZE = 32;
+	static const int CHAR_BIT = 8;
+public:
+	ibstream(const string &f):cnt(0) {open(f);}
+	~ibstream() {if (fin.is_open()) close();}
+	operator bool() {
+		return cnt != sum;
+	}
+	bool read_bit() {
+		if (!(cnt % SIZE)) read_buffer();
+		return buffer[cnt++ % SIZE];
+	}
+	unsigned char read_byte() {
+		bitset<CHAR_BIT> tmp;
+		for (int i = 0; i < CHAR_BIT; i++)
+			tmp[i] = read_bit();
+		return static_cast<unsigned char>(tmp.to_ulong());
+	}
+	void open(const string &f) {
+		fin.open(f, ios::binary | ios::in);
+		fin.read(reinterpret_cast<char *>(&sum), sizeof(sum));
+	}
+	void close() {fin.close();}
+private:
+	void read_buffer() {
+		unsigned long tmp;
+		fin.read(reinterpret_cast<char *>(&tmp), sizeof(tmp));
+		buffer = bitset<SIZE>(tmp);
+	}
+	unsigned long sum;
+	unsigned long cnt;
+	bitset<SIZE> buffer;
+	ifstream fin;
+};
+
 class obstream {
 	static const int SIZE = 32;
 	static const int CHAR_BIT = 8;
@@ -48,9 +86,16 @@ int main() {
 	bout.write_byte('e');
 	bout.write_byte('f');
 	bout.write_byte('g');
+	bout.write_byte('h');
+	bout.write_byte('i');
+	bout.write_bit(1);
+	bout.write_bit(0);
 	bout.close();
 	ibstream bin("sample.bin");
-	cout << bin.sum << endl;
+	for (int i = 0; i < 9; i++)
+		cout << bin.read_byte() << endl;
+	cout << bin.read_bit() << endl;
+	cout << bin.read_bit() << endl;
 	bin.close();
 	return 0;
 }
